d3d83adc2e4c220bfc291e5b9097e453ae3793db
dianomax <anishbudhathoki50@gmail.com>
2026-01-21 11:56:36 +0545

    changing to single workflow job build and deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div>Next.js on GitHub Pages &#128540; </div>
+      <div>Next.js on GitHub Pages &#128540;. deploying with single workflow jobs </div>
     </main>
   );
 }
