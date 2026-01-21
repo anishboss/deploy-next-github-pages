f048725ada50931bbfa40a6dcfdb66f9f7f1e251
dianomax <anishbudhathoki50@gmail.com>
2026-01-21 10:25:53 +0545

    add subdomain for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
+
+const gitHubPagesBasePath = process.env.PAGES_BASE_PATH;
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+  output: "export",
+  // basePath: process.env.PAGES_BASE_PATH,
+  ...(gitHubPagesBasePath ? { basePath: gitHubPagesBasePath } : {}),
 };
 
 export default nextConfig;
